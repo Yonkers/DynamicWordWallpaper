7face1ffb777d97a9ed5e76dd9be204d2d6e0362
Yonkers.Luo <lyjiang110711@gmail.com>
2013-01-11 18:09:10 +0800

    add app setting activity

--- src/com/yon/wallpaper/activity/Main.java
+++ src/com/yon/wallpaper/activity/Main.java
@@ -1,20 +1,26 @@
 package com.yon.wallpaper.activity;
 
-import com.yon.wallpaper.R;
-import com.yon.wallpaper.R.layout;
-import com.yon.wallpaper.R.menu;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.app.Activity;
 import android.view.Menu;
+
+import com.yon.wallpaper.R;
 
 public class Main extends PreferenceActivity {
 
+	private SharedPreferences prefs ;
+	
+	private String NAME_WALLPAPER_SETTING = "wallpaper_setting";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		//add preference
+		addPreferencesFromResource(R.xml.settings);
+		getPreferenceManager().findPreference(NAME_WALLPAPER_SETTING).setOnPreferenceClickListener(preferenceClickListener);
 	}
 
 	@Override
@@ -22,5 +28,17 @@ public class Main extends PreferenceActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	Preference.OnPreferenceClickListener preferenceClickListener = new Preference.OnPreferenceClickListener(){
 
+		@Override
+		public boolean onPreferenceClick(Preference preference) {
+			if(NAME_WALLPAPER_SETTING.equals(preference.getKey())){
+				Intent it = new Intent(Main.this,WallpaperSetting.class);
+				Main.this.startActivity(it);
+				return true;
+			}
+			return false;
+		}
+		
+	};
 }
